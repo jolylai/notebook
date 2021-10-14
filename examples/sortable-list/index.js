72902e5fee175057a5099d3718cbc0afcb625dce
jolylai <jolylai163@163.com>
2021-10-14 14:15:38 +0800

    feat: sortable

--- examples/sortable-list/index.js
+++ examples/sortable-list/index.js
@@ -1,74 +1,36 @@
-let dragStartIndex;
+function Sortable(el, options) {
+  this.el = el;
+  this.options = options;
+  this.addEventListeners(el);
+}
 
-const dragListItems = document.querySelectorAll('.draggable-list li');
+Sortable.prototype.onDragStart = function(event) {
+  console.log('event: ', event.type);
 
-function addEventListeners() {
-  const draggableElements = document.querySelectorAll('[draggable=true]');
+  const element = event.target;
+  const rec = element.getBoundingClientRect();
+  console.log('rec: ', rec);
 
-  for (let draggableElement of draggableElements) {
-    draggableElement.addEventListener('dragstart', onDragStart, false);
-    draggableElement.addEventListener('drag', onDrag, false);
-    draggableElement.addEventListener('dragend', onDragEnd, false);
+  const ghostElement = element.cloneNode(true);
+
+  ghostElement.style = `position: fixed; top: ${rec.top};left: ${rec.left};width: ${rec.width};height: ${rec.height}`;
+
+  this.parentElement.insertBefore(ghostElement, element);
+
+  console.log('this', this);
+
+  element.classList.add('list-item-drag');
+};
+
+Sortable.prototype.onClick = function(event) {
+  console.log('event: ', event.type);
+};
+
+Sortable.prototype.addEventListeners = function(el) {
+  for (let child of el.children) {
+    // child.addEventListener('click', this.onClick);
+    child.draggable = true;
+
+    child.addEventListener('dragstart', this.onDragStart);
   }
-
-  for (let element of dragListItems) {
-    element.addEventListener('dragover', onDragOver);
-    element.addEventListener('dragenter', onDragEnter);
-    element.addEventListener('dragleave', onDragLeave);
-    element.addEventListener('drop', onDrop);
-  }
-}
-
-addEventListeners();
-
-function onDragStart(event) {
-  console.log(event.type, event);
-
-  dragStartIndex = this.closest('[data-index]').getAttribute('data-index');
-}
-
-function onDrag(event) {
-  event.preventDefault();
-  // console.log(event.type, event);
-}
-
-function onDragEnd(event) {
-  // console.log(event.type, event);
-}
-
-function onDragEnter(event) {
-  event.preventDefault();
-  const element = event.target;
-
-  element.classList.add('list-item-over');
-}
-
-function onDragLeave(event) {
-  const element = event.target;
-
-  element.classList.remove('list-item-over');
-}
-
-function onDrop(event) {
-  const element = event.target;
-  element.classList.remove('list-item-over');
-
-  const dragEndIndex = this.getAttribute('data-index');
-  console.log('dragEndIndex: ', dragEndIndex);
-  swap(dragStartIndex, dragEndIndex);
-}
-
-function onDragOver(event) {
-  event.preventDefault();
-}
-
-// 互换位置
-function swap(fromIndex, toIndex) {
-  const formElement = dragListItems[fromIndex].querySelector(
-    '.list-item-content',
-  );
-  const toElement = dragListItems[toIndex].querySelector('.list-item-content');
-
-  dragListItems[fromIndex].appendChild(toElement);
-  dragListItems[toIndex].appendChild(formElement);
-}
+};
