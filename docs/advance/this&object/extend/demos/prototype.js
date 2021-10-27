42ea5b026bb54b7f2b8c76bb50552afce3995960
jolylai <jolylai163@163.com>
2021-10-27 17:36:52 +0800

    feat: extends

--- docs/advance/this&object/extend/demos/prototype.js
+++ docs/advance/this&object/extend/demos/prototype.js
@@ -15,3 +15,21 @@ Honda.prototype = new Car('Honda');
 const honda = new Honda('white');
 
 console.log('品牌', honda.getBranch());
+
+function SuperType() {
+  this.property = true;
+}
+
+SuperType.prototype.getSuperValue = function() {
+  return this.property;
+};
+
+function SubType() {
+  this.subProperty = false;
+}
+
+SuperType.prototype = new SuperType();
+
+SubType.prototype.getSubValue = function() {
+  return this.subProperty;
+};
